c6e103759a03f4c5e507107bd31e44cf01bd2236
Rajvardhanthakur <rajthakur3619@gmail.com>
2020-04-15 23:24:05 +0530

    adding project structure and creating a basic card

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrelloList from "./TrelloList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trello Clone</h1>
+      <TrelloList title="Test" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TrelloCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "@material-ui/core/Card"
+import Typhography from "@material-ui/core/Typography"
+
+const TrelloCard = () =>{
+    return(
+        <Card>
+            <Typhography gutterBottom>World of the day</Typhography>
+        </Card>
+    )
+}
+
+export default TrelloCard;
