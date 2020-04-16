39a1684d2a8271b7f1766a10f7fd6e9bac3b6d40
Rajvardhanthakur <rajthakur3619@gmail.com>
2020-04-16 11:54:50 +0530

    adding some style to cards

--- src/components/TrelloCard.js
+++ src/components/TrelloCard.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
 import Typhography from "@material-ui/core/Typography"
 
 const TrelloCard = ({text}) =>{
-    return(
-        <Card>
-            <Typhography gutterBottom>{text}</Typhography>
-        </Card>
-    )
+    return (
+      <Card style={styles.cardContainer}>
+        <CardContent>
+          <Typhography gutterBottom>{text}</Typhography>
+        </CardContent>
+      </Card>
+    );
+}
+
+const styles = {
+    cardContainer : {
+        marginBottom: 8
+    }
 }
 
 export default TrelloCard;
