8cc3afb91a2d9624bc598eed1545cd264c2c2fc5
Rajvardhanthakur <rajthakur3619@gmail.com>
2020-04-18 19:42:34 +0530

    Saving card after dropping

--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -51,27 +51,50 @@ const listReducer = (state = initalState, action) =>{
            return [...state, newList]
         
         case CONSTANTS.ADD_CARD:
-          const newCard = {
-            text: action.payload.text,
-            id: `card-${cardID}`,
-          };
-          cardID += 1;
+          {
+            const newCard = {
+              text: action.payload.text,
+              id: `card-${cardID}`,
+            };
+            cardID += 1;
 
-          console.log("action recieved", action)
+            console.log("action recieved", action);
 
-          const newState = state.map(list => {
-            if(list.id === action.payload.listID){
-              return {
-                ...list,
-                cards : [...list.cards, newCard]
+            const newState = state.map((list) => {
+              if (list.id === action.payload.listID) {
+                return {
+                  ...list,
+                  cards: [...list.cards, newCard],
+                };
+              } else {
+                return list;
               }
-            }else{
-              return list;
+            });
+
+            return newState;
+          }
+
+        case CONSTANTS.DRAG_HAPPENED:
+        
+            const {
+              droppableIdStart,
+              droppableIdEnd,
+              droppableIndexStart,
+              droppableIndexEnd,
+              draggableId,
+            } = action.payload;
+
+            const newState = [...state];
+
+            //Drag and Drop in the same listStyle
+            if (droppableIdStart === droppableIdEnd) {
+              const list = state.find(list => droppableIdStart === list.id);
+              const card = list.cards.splice(droppableIndexStart, 1);
+              list.cards.splice(droppableIndexEnd, 0, ...card);
             }
-          });
 
-          return newState;
-
+            return newState;
+          
         default:
             return state;
     }
